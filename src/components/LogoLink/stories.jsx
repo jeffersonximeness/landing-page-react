18b8bddf016fef9e7cbb8a9704cdeb791f3983e8
jeffersonximeness <jeffersonximenes1@gmail.com>
2022-02-01 14:32:22 -0300

    Componente LogoLink e testes

---
+++ src/components/LogoLink/stories.jsx
@@ -0,0 +1,34 @@
+import { LogoLink } from '.'
+
+export default {
+  title: 'LogoLink',
+  componente: LogoLink,
+
+  args: {
+    text: 'LogoLink',
+    srcImg: 'assets/images/logo.svg',
+    link: 'http://localhost'
+  }
+}
+
+export const ImageOnly = (args) => {
+
+  return (
+    <div>
+      <LogoLink {...args} />
+    </div>
+  )
+}
+
+export const TextOnly = (args) => {
+
+  return (
+    <div>
+      <LogoLink {...args} />
+    </div>
+  )
+}
+
+TextOnly.args = {
+  srcImg: ''
+}
