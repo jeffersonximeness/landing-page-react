e881c49841bf5ac2e12f36ea8e0e092cc49552ab
jeffersonximeness <jeffersonximenes1@gmail.com>
2022-02-01 10:47:00 -0300

    Componente link com testes

---
+++ src/components/MenuLink/MenuLink.test.jsx
@@ -0,0 +1,35 @@
+import { render, screen } from '@testing-library/react'
+import { renderTheme } from '../../styles/render-theme'
+import { MenuLink } from '.'
+
+
+describe('<MenuLink />', () => {
+  it('should render a link', () => {
+    renderTheme(
+      <MenuLink
+        link="http://localhost"
+      >
+        Children
+      </MenuLink>
+    )
+
+    expect(screen.getByRole('link', { name: 'Children' })).toBeInTheDocument()
+  })
+
+  it('should open in a new tab', () => {
+    renderTheme(
+      <MenuLink
+        link="http://localhost"
+        newTab={true}
+      >
+        Children
+      </MenuLink>
+    )
+
+    expect(screen.getByRole('link', { name: 'Children' })).toHaveAttribute(
+      'target',
+      '_blank'
+    )
+  })
+
+})
