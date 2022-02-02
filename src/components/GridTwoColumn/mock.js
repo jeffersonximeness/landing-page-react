13465768abdede5b19863cbbef934cb414de612d
jeffersonximeness <jeffersonximenes1@gmail.com>
2022-02-01 21:34:23 -0300

    Componente de grid de duas colunas criado e testes

---
+++ src/components/GridTwoColumn/mock.js
@@ -0,0 +1,9 @@
+export default {
+  title: 'Grid Two Colum',
+  text: `Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Cum ullam accusantium exercitationem libero ipsa facere, repellat tenetur excepturi asperiores quisquam dolor non amet voluptatum
+        quas veniam maxime sint itaque? Voluptate fuga voluptas, perferendis molestias beatae recusandae, magnam similique cum repellat impedit
+        dolorem suscipit harum neque excepturi blanditiis, itaque corrupti doloremque! Autem, corporis nam. Molestias rem tempora impedit,
+        quisquam doloremque ea!`,
+  srcImg: 'assets/images/javascript.svg'
+}
